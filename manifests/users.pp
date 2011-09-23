0cd5e2118d534273dc0259abb4a25ca93fdbedb2
Kiall Mac Innes <kiall@managedit.ie>
2011-09-23 19:16:13 +0100

    Add zombor+samsoir SSH keys

--- manifests/users.pp
+++ manifests/users.pp
@@ -70,4 +70,18 @@ ssh_authorized_key {
 		key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAvR3hLb7gdXHGLWEu3Rt9hBpfidN7MXO/HOnX8BBoaz9RfrEr68SlKlKLpZ8MInUrOeCTq2E5NQpgcN7hPRh/aFM2oHpYPBx6z0EroaMROWJtujDEi5U1lWFKhywROMA5VTgkcizA11mjTABonccLiirzjPh0DgCU4bUMoKJpGfzNU/OJweyqDvEgyLib2NDL1ntrShzx5gkgc/24QliBhiu15ZBYXnrU1VrMF7SY13tB7TdEOC+/qpHU7Zz8TNLjzLoFKo/LSZWA6ZWagZj0YmLXj6R2CX1jvvaJiorQ1Z/neJ8EU1Qy+9qLKfYCa+QiJP9/RUaMhG0dFtRFkiT6YQ==",
 		options => [],
 		require => User["brmatt"];
+	"contractfrombelow@gmail.com":
+		ensure  => present,
+		user    => "zombor",
+		type    => "ssh-rsa",
+		key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEA0QY0KaYcYQ7F2FJgvGbJm1rSP1HQo5XYxrl+rI6DqAFWjnNp7kq/9zeSJ4GdUYPNXDpoDUnyg7Z/mD2wB36/0vF2yf9GTqcMaWRuf8rEPd6GTLGjFStLKn0kniZFDZpeIYl89c2GSmZUnWqY8UfQEyHGzVweB11RglTQJuIvi1uhNbLsSNsDfb9hzYWaNGf77SVPCbOVLTN76xd0EZmonPxyDSIS+SHEGkNHEMpuqQEV/Es5fZ/d+jGmMbOwToeszpfs7FiWVFDBn+WHLpiYHixFrhf/dHWaf2Rvf/wmobgXxIph8gI3VzzN934BTDe9a24jxaezwQO3BqQGIlR5tw==",
+		options => [],
+		require => User["zombor"];
+	"sam@def.reyssi.net":
+		ensure  => present,
+		user    => "samsoir",
+		type    => "ssh-rsa",
+		key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAwpzZ9IwKtKZXkVasS+FBod2Qo+mxLPSyQ4qZrEzE+LQiBoMMol9guJHgOLbMmH0ekL4tMm3e67oZaIaW/XoLeBPi86VnTXxw8UekJaQbpZNrCOxDuolvBqvsxJpYguw7TIJOnEKrdOfPPCgdAvfn3C24Ck+PL6LZ3TGAvreKqY7khhuEo5lKQayRhBnO3whmzh9ijnhYNuLV/c3uxQaWygm02jqqrhzM/BtNtpV/CjcGJdFIt96UFXdQPCS2rx+KEA0nR1uL0YuG1RDK2GoKmN8WG/jrdlQ08fcXZtoCHePI8eUG2hlMYO1pjLoiL/ZLB3mwkahGlZxK58bUmdNf2w==",
+		options => [],
+		require => User["samsoir"];
 }
